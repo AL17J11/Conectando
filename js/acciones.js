8518924e708ea3a6ea72af747f92ae21a2b850db
Victor Guevara <guevamartvic@gmail.com>
2013-06-14 09:29:19 -0500

    1.5

---
+++ js/acciones.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+function LoginConectar (U, P)
+{
+	datos="usuario="+U+"&password="+P;
+	$.ajax({
+		type:"POST",
+		url:"http://192.168.1.167/Sitio6/Login_in_m.php",
+		data:datos
+	}).done(function(msg){
+		alert(msg);
+		if(msg=="" || msg==null)
+		{
+			alert("usuario incorrecto");}
+			else
+			{
+				var OUsuario=jQuery.pareseJSON(msg);
+				ID_Usuario=OUsuario.Usuario;
+				alert("Bienvenido "+ID_Usuario);
+				location.href="#Busqueda";
+			}
+	});
+}
+	$(document).ready(function(e){
+		document.addEventListener("deviceready",function(){
+			$('.Enviar').tap(function(){
+				var formulario=$(this).parents('form');
+				var usuario=document.getElementById('Usuario').value;
+				var password=document.getElementById('Password').value;
+				LoginConectar(usuario,password);
+			});
+		});
+	});
